86c4f15e424d5aee6aeeba62e3606fdb690812f1
Egor A. Blinov <escaton@dhcp-202-19-wifi.yandex.net>
2013-01-26 15:05:25 +0400

    неработающий css

---
+++ blocks-desktop/b-body/b-body.priv.js
@@ -0,0 +1,15 @@
+blocks['b-body'] = function(data) {
+    return {
+        block: 'b-body',
+        content: [
+            {
+                elem: 'left',
+                content: [] //blocks['b-menu'](data)
+            },
+            {
+                elem: 'right',
+                content: [] // blocks['b-content'](data)
+            }
+        ]
+    }
+}
