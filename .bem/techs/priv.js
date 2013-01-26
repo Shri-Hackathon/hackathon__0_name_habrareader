76bfd1b58ff238a61a570ad2cab65087afdb7aec
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-01-26 13:42:06 +0400

    добавил описание тезноологии priv.js

---
+++ .bem/techs/priv.js
@@ -0,0 +1,16 @@
+var BEM = require('bem');
+
+exports.baseTechPath = BEM.require.resolve('./techs/js.js');
+exports.techMixin = {
+    getSuffixes: function() {
+        return ['priv.js'];
+    },
+
+    getBuildResult: function(prefixes, suffix, outputDir, outputName) {
+        return this.__base.apply(this, arguments)
+            .then(function(res) {
+                res.unshift('var blocks = module.exports;');
+                return res;
+            });
+    }
+};
